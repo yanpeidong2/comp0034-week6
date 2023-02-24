24da33756ae1f47d694af5585fecd71460792264
nicholsons <sarah.sanders@ucl.ac.uk>
2023-02-24 15:06:21 +0000

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="comp0034-week6",
-    packages=["src.flask_app", find_packages()],
+    packages=["flask_app", find_packages()],
     package_dir={"": "src"},
     include_package_data=True,
     install_requires=["flask", "pandas", "openpyxl"],
