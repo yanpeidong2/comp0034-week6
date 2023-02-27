31d4a3fe52571e2f06ef11dbf4cca70148dea2e2
nicholsons <sarah.sanders@ucl.ac.uk>
2023-02-27 09:24:28 +0000

    removed the src package to avoid confusion

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup
 setup(
     name="comp0034-week6",
     packages=["flask_app"],
-    package_dir={"": "src"},
     include_package_data=True,
     install_requires=["flask"],
 )
